505863f3c24fc3d97b82bfb6f9c712d7bd7d4909
eduardo ciudad figueredo <eduardociudadfigueredo@gmail.com>
2026-04-21 14:26:45 -0300

    adicionei tokens e seguindo com o curso da alura

--- src/main/java/br/com/forum_hub/infra/seguranca/ConfigSegurity.java
+++ src/main/java/br/com/forum_hub/infra/seguranca/ConfigSegurity.java
@@ -3,6 +3,8 @@ package br.com.forum_hub.infra.seguranca;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -24,5 +26,10 @@ public class ConfigSegurity {
     @Bean
     public PasswordEncoder encriptador() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager atenticationManeger(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
