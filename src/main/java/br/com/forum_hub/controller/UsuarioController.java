d71353d23029880bff9d3ab16a715068ac851e8c
eduardo ciudad figueredo <eduardociudadfigueredo@gmail.com>
2026-04-25 14:30:48 -0300

    adicionei os dtos de usuario e o controller

--- src/main/java/br/com/forum_hub/controller/UsuarioController.java
+++ src/main/java/br/com/forum_hub/controller/UsuarioController.java
@@ -1,8 +1,24 @@
 package br.com.forum_hub.controller;
 
+import br.com.forum_hub.domain.topico.DadosCadastroTopico;
+import br.com.forum_hub.domain.topico.DadosListagemTopico;
+import br.com.forum_hub.domain.usuario.Usuario;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 public class UsuarioController {
 
+
+    @PostMapping("/registrar")
+    public ResponseEntity<DadosListagemUsuario> cadastrar(@RequestBody @Valid DadosCadastroUsuario dados, UriComponentsBuilder uriBuilder) {
+        var usuario = service.cadastrar(dados);
+        var uri = uriBuilder.path("/{nomeUsuario}").buildAndExpand(usuario.getNomeUsuario).toUri();
+        return ResponseEntity.created(uri).body(new DadosListagemUsuaio(usuario));
+    }
 }
