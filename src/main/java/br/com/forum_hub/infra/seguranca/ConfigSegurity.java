3dfb53873d15ff4fd6ee4e6593504facc2f17d7a
eduardo ciudad figueredo <eduardociudadfigueredo@gmail.com>
2026-05-03 14:30:52 -0300

    adicionei todas as permições de usuario

--- src/main/java/br/com/forum_hub/infra/seguranca/ConfigSegurity.java
+++ src/main/java/br/com/forum_hub/infra/seguranca/ConfigSegurity.java
@@ -3,6 +3,7 @@ package br.com.forum_hub.infra.seguranca;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -32,6 +33,16 @@ public class ConfigSegurity {
                         req -> {
 
                             req.requestMatchers("/login", "/atualizar-token", "/registrar", "/verificar-conta").permitAll();
+
+                            req.requestMatchers(HttpMethod.GET ,"/cursos").permitAll();
+                            req.requestMatchers(HttpMethod.GET ,"/topicos/**").permitAll();
+                            req.requestMatchers(HttpMethod.POST ,"/topicos").hasRole("ESTUDANTE");
+                            req.requestMatchers(HttpMethod.PUT, "/topicos").hasRole("ESTUDANTE");
+                            req.requestMatchers(HttpMethod.DELETE ,"/topicos/**").hasRole("ESTUDANTE");
+                            req.requestMatchers(HttpMethod.PATCH ,"/topicos/**").hasRole("MODERADOR");
+
+                            req.requestMatchers(HttpMethod.PATCH ,"/adicionar-perfil/**").hasRole("ADMIN");
+
                             req.anyRequest().authenticated();
                         }
                 )
